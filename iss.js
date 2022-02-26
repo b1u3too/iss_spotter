eb89ed22386552f9b721c38c18add13fb5474d37
b1u3too <almadsen792@live.ca>
2022-02-26 22:29:16 +0000

    implement fetCoordsByIP

--- iss.js
+++ iss.js
@@ -26,4 +26,21 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code: ${response.statusCode} when fetching coordinates. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    callback(null, { latitude: data.latitude, longitude: data.longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
