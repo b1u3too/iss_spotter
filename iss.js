2d04076e6a74b9d6d8259e943f718e6b91990990
b1u3too <almadsen792@live.ca>
2022-02-26 23:43:28 +0000

    implement nextISSTimesForMyLocation

--- iss.js
+++ iss.js
@@ -60,4 +60,38 @@ const fetchISSFlyOverTimes = function(coords, callback) {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results. 
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */ 
+ const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    fetchCoordsByIP(ip, (error, coords) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+
+      fetchISSFlyOverTimes(coords, (error, times) => {
+        if (error) {
+          callback(error, null);
+          return;
+        }
+
+        callback(null, times);
+      })
+    })
+  })
+}
+
+module.exports = { nextISSTimesForMyLocation };
