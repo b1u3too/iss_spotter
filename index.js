ac38319d4d964e1210264151bf53105d1d1e3628
b1u3too <almadsen792@live.ca>
2022-02-26 22:50:36 +0000

    add fetchIssFlyOverTimes driving call

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
-//     console.log("It didn't work!", error);
+//     console.log("It broke!", error);
 //     return;
 //   }
 
 //   console.log("It worked! Returned IP: ", ip);
 // });
 
-fetchCoordsByIP('potato', (error, coords) => {
-  if (error) {
-    console.log("It broke while finding coordinates!", error);
-    return;
-  }
+// fetchCoordsByIP('potato', (error, coords) => {
+//   if (error) {
+//     console.log("It broke!", error);
+//     return;
+//   }
 
-  console.log("It worked! Found coordinates: ", coords);
-});
+//   console.log("It worked! Found coordinates: ", coords);
+// });
+
+// fetchISSFlyOverTimes({ "latitude": 0,
+// "longitude": 0 }, (error, times) => {
+//   if (error) {
+//     console.log("It broke!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Flyby details: ", times);
+// });
