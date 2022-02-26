75f26c1e5f149ea66e7bf380513d1cb3efd7bf19
b1u3too <almadsen792@live.ca>
2022-02-26 22:28:44 +0000

    import fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned IP: ", ip);
+// });
+
+fetchCoordsByIP('potato', (error, coords) => {
   if (error) {
-    console.log("It didn't work!", error);
+    console.log("It broke while finding coordinates!", error);
     return;
   }
 
-  console.log("It worked! Returned IP: ", ip);
+  console.log("It worked! Found coordinates: ", coords);
 });
